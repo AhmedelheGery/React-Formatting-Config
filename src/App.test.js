3145ff523851b87e2bbffb4c7d322d1b2f024443
Ahmed Elhegery <ahmed.3laa.2020@gmail.com>
2020-08-11 00:13:52 +0200

    install enzyme and add our first test for app js component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+let wrapped;
+
+beforeEach(() => {
+  wrapped = shallow(<App />);
 });
+
+it("has an input", () => {
+  expect(wrapped.find('input').length).toEqual(1);
+});
+
